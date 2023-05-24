4e6f323fef2d3a11e1893aede135b5e2a1a59f60
Joon-yong Hong <joonyong.hong@lge.com>
2023-05-24 15:27:05 +0900

    list 2.15

--- list2/src/main/kotlin/example/Main.kt
+++ list2/src/main/kotlin/example/Main.kt
@@ -17,6 +17,7 @@ fun main(args: Array<String>) {
   println(Color.BLUE.rgb())
   println(getMnemonic(Color.BLUE))
   println(getWarmth(Color.ORANGE))
+  println(mix(Color.BLUE, Color.YELLOW))
 }
 
 fun getMnemonic(color: Color) =
@@ -35,3 +36,11 @@ fun getWarmth(color: Color) = when(color) {
   Color.GREEN -> "neutral"
   Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
 }
+
+fun mix(c1: Color, c2: Color) =
+  when(setOf(c1, c2)) {
+    setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+    setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
+    setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+    else -> throw Exception("Dirty color")
+  }
