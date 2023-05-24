9864ac5e98e15215d0d2e82d27012d1fdbf63d7a
Joon-yong Hong <joonyong.hong@lge.com>
2023-05-24 16:43:44 +0900

    list 2.22

---
+++ list2/src/main/kotlin/example/Iteration.kt
@@ -0,0 +1,14 @@
+package example
+
+fun fizzBuzz(i: Int) = when {
+  i % 15 == 0 -> "FizzBuzz "
+  i % 3 == 0 -> "Fizz "
+  i % 5 == 0 -> "Buzz "
+  else -> "$i "
+}
+
+fun iter1() {
+  for(i in 1..100) {
+    print(fizzBuzz(i))
+  }
+}
--- list2/src/main/kotlin/example/Main.kt
+++ list2/src/main/kotlin/example/Main.kt
@@ -22,4 +22,6 @@ fun main(args: Array<String>) {
 
   println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
   println(eval2(Sum(Sum(Num(1), Num(2)), Num(4))))
+
+  iter1()
 }
