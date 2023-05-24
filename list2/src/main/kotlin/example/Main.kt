acd0ff76cfd9e8bd51956c2c9ab7e93b1d7ac037
Joon-yong Hong <joonyong.hong@lge.com>
2023-05-24 15:35:44 +0900

    list 2.16
    list 2.16

--- list2/src/main/kotlin/example/Main.kt
+++ list2/src/main/kotlin/example/Main.kt
@@ -18,6 +18,7 @@ fun main(args: Array<String>) {
   println(getMnemonic(Color.BLUE))
   println(getWarmth(Color.ORANGE))
   println(mix(Color.BLUE, Color.YELLOW))
+  println(mixOptimized(Color.BLUE, Color.YELLOW))
 }
 
 fun getMnemonic(color: Color) =
@@ -42,5 +43,13 @@ fun mix(c1: Color, c2: Color) =
     setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
     setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
     setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+    else -> throw Exception("Dirty color")
+  }
+
+fun mixOptimized(c1: Color, c2: Color) =
+  when {
+    (c1 == Color.RED && c2 == Color.YELLOW) || (c1 == Color.YELLOW && c2 == Color.RED) -> Color.ORANGE
+    (c1 == Color.YELLOW && c2 == Color.BLUE) || (c1 == Color.BLUE && c2 == Color.YELLOW) -> Color.GREEN
+    (c1 == Color.BLUE && c2 == Color.VIOLET) || (c1 == Color.VIOLET && c2 == Color.BLUE) -> Color.INDIGO
     else -> throw Exception("Dirty color")
   }
