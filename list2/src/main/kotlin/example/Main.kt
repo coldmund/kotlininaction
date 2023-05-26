6bb3852ea0d88fbd9de045841a13b75f682f0302
Joon-yong Hong <joonyong.hong@lge.com>
2023-05-26 11:47:55 +0900

    list 2.26

--- list2/src/main/kotlin/example/Main.kt
+++ list2/src/main/kotlin/example/Main.kt
@@ -31,4 +31,14 @@ fun main(args: Array<String>) {
   fun isNotDigit(c: Char) = c !in '0'..'9'
   println(isLetter('q'))
   println(isNotDigit('x'))
+
+  fun recognize(c: Char) = when(c) {
+    in '0'..'9' -> "It's a digit!"
+    in 'a'..'z', in 'A'..'Z' -> "It's a letter!"
+    else -> "I don't know.."
+  }
+  println(recognize('8'))
+
+  println("Kotlin" in "Java".."Scala")
+  println("Kotlin" in setOf("Java", "Scala"))
 }
